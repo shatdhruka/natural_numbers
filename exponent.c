251a27dd4254b5b27f172f4c65792eefa47a49db
shatdhruka <42232515+shatdhruka@users.noreply.github.com>
2019-01-15 20:34:14 +0530

    Create exponent.c

---
+++ exponent.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int N, K, result;
+    printf("");
+    scanf("%d", &N);
+    printf("");
+    scanf("%d", &K);
+    result = pow(N, K);
+    printf("%d",result);
+    return 0;
+}
